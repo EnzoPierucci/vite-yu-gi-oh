264658001731983a8cc9a2a8b0a7093817993a8c
EnzoPierucci <enzopierucci@gmail.com>
2024-02-12 15:16:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import CardComp from "./components/CardComp.vue" 
+import LoaderComp from "./components/LoaderComp.vue"  
+
+export default {
+  components:{
+  CardComp,
+  LoaderComp,
+
+  }
+}  
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
